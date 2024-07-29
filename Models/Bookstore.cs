379a349ee1dd6f611294232c71bb9cfb57a90b86
Aizar Rahima <aizar.works@gmail.com>
2024-07-29 21:45:58 +0700

    Add project files.

---
+++ Models/Bookstore.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Bookstore.Models;
+
+namespace AspNetCoreWebAPI8.Models
+{
+    public class Bookstore:DbContext
+    {
+        public Bookstore(DbContextOptions<Bookstore> options) : base(options) 
+        {
+            
+        }
+
+        public DbSet<Book> Books { get; set; } = null!;
+    }
+}
