55d7b5b91892bb1bd910519b8d305d3b5d6e5ab7
Aizar Rahima <aizar.works@gmail.com>
2024-07-30 12:16:18 +0700

    update models

--- Models/Book.cs
+++ Models/Book.cs
@@ -1,10 +1,21 @@
-﻿namespace Bookstore.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Bookstore.Models
 {
     public class Book
     {
         public int Id { get; set; }
-        public string? Title { get; set; }
-        public string? Categories { get; set; }
-        public DateTime ReleaseDate { get; set; }
+        [Required]
+        public int AuthorId { get; set; }
+        [Required]
+        [StringLength(100)]
+        public string Title { get; set; }
+        [Required]
+        [StringLength(100)]
+        public string Categories { get; set; }
+        [Required]
+        [StringLength(100)]
+        public string publisher { get; set; }
+        public virtual Author Author { get; set; } // Lazy loading
     }
 }
--- Models/Bookstore_DB.cs
+++ Models/Bookstore_DB.cs
@@ -7,9 +7,11 @@ namespace Bookstore.Models
     {
         public Bookstore_DB(DbContextOptions<Bookstore_DB> options) : base(options) 
         {
-            
+            // Enable Lazy Loading
+            this.ChangeTracker.LazyLoadingEnabled = true;
         }
 
         public DbSet<Book> Books { get; set; } = null!;
+        public DbSet<Author> Authors { get; set; } = null!;
     }
 }
