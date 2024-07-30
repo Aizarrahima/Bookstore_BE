3d16e5cac2fef5279b9db60d8e187a9a9b562219
Aizar Rahima <aizar.works@gmail.com>
2024-07-30 12:16:08 +0700

    update contoller

---
+++ Controllers/AuthorsController.cs
@@ -0,0 +1,122 @@
+﻿using Bookstore.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bookstore.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthorsController : ControllerBase
+    {
+        private readonly Bookstore_DB _bookstore;
+
+        public AuthorsController(Bookstore_DB bookstore)
+        {
+            _bookstore = bookstore;
+        }
+
+        #region Get: api/Authors
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Author>>> GetAuthors()
+        {
+            if (_bookstore.Authors == null)
+            {
+                return NotFound();
+            }
+
+            return await _bookstore.Authors.ToListAsync();
+        }
+        #endregion
+
+        #region Get: api/Authors/{Id}
+        [HttpGet("{Id}")]
+        public async Task<ActionResult<Author>> GetAuthor(int Id)
+        {
+            if (_bookstore.Authors == null)
+            {
+                return NotFound();
+            }
+
+            var author = await _bookstore.Authors.FindAsync(Id);
+
+            if (author == null)
+            {
+                return NotFound();
+            }
+            return author;
+        }
+        #endregion
+
+        #region Post: api/Authors
+        [HttpPost]
+        public async Task<ActionResult<Author>> CreateAuthor(Author author)
+        {
+            _bookstore.Authors.Add(author);
+            await _bookstore.SaveChangesAsync();
+            return CreatedAtAction(nameof(GetAuthor), new { Id = author.Id }, author);
+        }
+        #endregion
+
+        #region Put: api/Authors/{Id}
+        [HttpPut("{Id}")]
+        public async Task<IActionResult> UpdateAuthor(int Id, Author author)
+        {
+            if (Id != author.Id)
+            {
+                return BadRequest();
+            }
+
+            _bookstore.Entry(author).State = EntityState.Modified;
+            try
+            {
+                await _bookstore.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!AuthorExists(Id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            var updatedAuthor = await _bookstore.Authors.FindAsync(Id);
+
+            return Ok(updatedAuthor);
+        }
+
+        private bool AuthorExists(int Id)
+        {
+            return (_bookstore.Authors?.Any(author => author.Id == Id)).GetValueOrDefault();
+        }
+        #endregion
+
+        #region Delete: api/Authors/{Id}
+        [HttpDelete("{Id}")]
+        public async Task<IActionResult> AuthorDelete(int Id)
+        {
+            if (_bookstore.Authors == null)
+            {
+                return NotFound();
+            }
+
+            var author = await _bookstore.Authors.FindAsync(Id);
+            if (author == null)
+            {
+                return NotFound();
+            }
+
+            _bookstore.Authors.Remove(author);
+            await _bookstore.SaveChangesAsync();
+            return Ok(new { message = "Delete success." });
+        }
+        #endregion
+    }
+}
--- Controllers/BooksController.cs
+++ Controllers/BooksController.cs
@@ -23,7 +23,9 @@ namespace Bookstore.Controllers
             {
                 return NotFound();
             }
-            return await _bookstore.Books.ToListAsync();
+            return await _bookstore.Books
+                                   .Include(book => book.Author) // Eager Loading
+                                   .ToListAsync();
         }
         #endregion
 
@@ -31,12 +33,15 @@ namespace Bookstore.Controllers
         [HttpGet("{Id}")]
         public async Task<ActionResult<Book>> GetBook(int Id)
         {
-            if (_bookstore.Books == null) { 
-            return NotFound();
+            if (_bookstore.Books == null)
+            {
+                return NotFound();
             }
 
-            var book = await _bookstore.Books.FindAsync(Id);
-            if (book is null) 
+            var book = await _bookstore.Books
+                                      .Include(book => book.Author) // Eager Loading
+                                      .FirstOrDefaultAsync(book => book.Id == Id);
+            if (book is null)
             {
                 return NotFound();
             }
@@ -48,6 +53,13 @@ namespace Bookstore.Controllers
         [HttpPost]
         public async Task<ActionResult<Book>> CreateBook(Book book)
         {
+            var existingAuthor = await _bookstore.Authors.FindAsync(book.AuthorId);
+            if (existingAuthor == null)
+            {
+                return BadRequest("Author does not exist.");
+            }
+            book.Author = existingAuthor;
+
             _bookstore.Books.Add(book);
             await _bookstore.SaveChangesAsync();
             return CreatedAtAction(nameof(GetBook), new { Id = book.Id }, book);
@@ -55,12 +67,22 @@ namespace Bookstore.Controllers
         #endregion
 
         #region Put: api/Books/{Id}
-        [HttpPut]
-        public async Task<ActionResult<Book>> BookUpdate(int Id, Book book)
+        [HttpPut("{Id}")]
+        public async Task<IActionResult> BookUpdate(int Id, Book book)
         {
             if (Id != book.Id)
             {
-                return BadRequest();
+                return BadRequest("Book not found.");
+            }
+
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (!AuthorExists(book.AuthorId))
+            {
+                return NotFound(new { Message = "Author not found." });
             }
 
             _bookstore.Entry(book).State = EntityState.Modified;
@@ -79,11 +101,20 @@ namespace Bookstore.Controllers
                     throw;
                 }
             }
-            return NoContent();
+
+            var updatedBook = await _bookstore.Books.Include(b => b.Author).FirstOrDefaultAsync(b => b.Id == Id);
+
+            return Ok(updatedBook);
         }
 
-        private bool BookExists(int Id) {
+        private bool BookExists(int Id)
+        {
             return (_bookstore.Books?.Any(book => book.Id == Id)).GetValueOrDefault();
+        }
+
+        private bool AuthorExists(int authorId)
+        {
+            return (_bookstore.Authors?.Any(author => author.Id == authorId)).GetValueOrDefault();
         }
         #endregion
 
@@ -91,20 +122,20 @@ namespace Bookstore.Controllers
         [HttpDelete("{Id}")]
         public async Task<ActionResult<Book>> BookDelete(int Id)
         {
-            if(_bookstore.Books is null)
+            if (_bookstore.Books is null)
             {
                 return NotFound();
             }
 
             var book = await _bookstore.Books.FindAsync(Id);
-            if(book is null)
+            if (book is null)
             {
                 return NotFound();
             }
 
             _bookstore.Books.Remove(book);
             await _bookstore.SaveChangesAsync();
-            return NoContent();
+            return Ok(new { message = "Delete success." });
         }
         #endregion
     }
